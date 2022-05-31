5d8f24c6d8f9cae8827884c4f0feca7681ca1a81
devispei <devis@terraview.co>
2022-06-01 00:23:17 +0800

    OK

---
+++ src/pages/member/effective_pt.tsx
@@ -0,0 +1,438 @@
+import Row from '@paljs/ui/Row';
+import Col from '@paljs/ui/Col';
+import React from 'react';
+import Layout from 'Layouts';
+import 'react-calendar/dist/Calendar.css';
+import { breakpointDown } from '@paljs/ui/breakpoints';
+import { createGlobalStyle, css } from 'styled-components';
+import { Radio } from '@paljs/ui/Radio';
+import { Checkbox } from '@paljs/ui/Checkbox';
+import { Button } from '@paljs/ui/Button';
+const AgentReport = () => {
+  const CustomCSS = createGlobalStyle`
+${() => css`
+  #contentTableObj {
+    border: 0px solid black;
+    border-collapse: collapse;
+  }
+  #contentTable tr {
+    height: 35px;
+    line-height: 35px;
+    border-radius: 5px;
+    border: 0px solid white;
+    position: relative;
+  }
+  #contentTable tr {
+    background-color: #d7f5eb;
+    width: 50%;
+    color: #051139;
+  }
+  #contentTableObj,
+  #Main-contentTableObj {
+    width: 100%;
+  }
+
+  .auth-layout .main-content {
+    background-color: #e7e8ee;
+    padding-top: 1rem;
+  }
+  .contentHeaderImage {
+    margin-bottom: 0rem;
+  }
+  div.header-white {
+    height: 50px;
+    background: url(/images/white_table_head.png);
+    background-size: cover;
+    display: flex;
+  }
+  div.blue-title {
+    margin: auto;
+    width: 334px;
+    height: 33px;
+    margin-top: 0px;
+    background: url(/images/black_title_bkg.png);
+    background-size: 334px 33px;
+    line-height: 33px;
+    font-weight: bold;
+  }
+  div.tabs {
+    left: 0px;
+    bottom: 0;
+    width: 100%;
+    max-width: 800px;
+  }
+  div.tabs div.tab {
+    cursor: pointer;
+    max-width: 200px;
+    padding: 1rem;
+    float: left;
+    background: white;
+    border-top-left-radius: 11px;
+    border-top-right-radius: 11px;
+    border: 1px solid gray;
+    color: gray;
+    text-align: center;
+  }
+  div.tabs div.tab.selected {
+    background: url(/images/sales/tabcell_bkg.png);
+    background-size: cover;
+    color: white;
+    padding-top: calc(1rem + 2px);
+    border: 0px solid white;
+  }
+  div.content-area {
+    width: 100%;
+    background: white;
+    padding: 1rem 1rem 4rem 1rem;
+  }
+  div.content-area * {
+    color: #051139;
+  }
+  div.content-title {
+    color: #0071ff;
+  }
+
+  tr.titleimg {
+    background: url(/images/sales/header_row.png);
+    background-size: 100% 100%;
+  }
+  div.grayRow {
+    background: #dceffd;
+    box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.07);
+    border-radius: 10px;
+    width: 100%;
+  }
+  div.grayRow:nth-child(2n + 1) {
+    background: #d7f5eb;
+  }
+  div.grayRow table {
+    width: 100%;
+  }
+  div.form-item div.form-label {
+    min-width: 0%;
+  }
+  div.mobile {
+    display: none;
+  }
+  div.desktop {
+    display: block;
+  }
+  .react-calendar {
+    height: 305px;
+    background: #14235b;
+  }
+  .react-calendar * {
+    color: white !important;
+  }
+  .react-calendar__tile:enabled:hover {
+    background-color: white;
+    border-radius: 20px;
+    color: #0075ff;
+  }
+  .react-calendar__tile--active {
+    border-radius: 20px;
+  }
+  .react-calendar__tile:enabled:hover * {
+    color: #0075ff !important;
+  }
+  .react-calendar__tile--now,
+  .react-calendar__tile--now:enabled:hover,
+  .react-calendar__tile--now:enabled:focus {
+    background: linear-gradient(89.33deg, #0075ff 0.58%, #00d1ff 104.03%);
+    background-size: cover;
+    border-radius: 20px;
+  }
+  ${breakpointDown('sm')`
+    div.mobile {
+        display: block;
+    }
+    div.desktop {
+        display: none;
+    }
+  `}
+
+  div.middle-title-area {
+    background: url(/images/sales/agent_group_header_bkg.png);
+    background-size: cover;
+    padding: 2rem;
+    display: flex;
+    flex-direction: row;
+    color: white;
+  }
+  div.middle-title-area div.button:hover {
+    background: #fff3;
+  }
+  div.middle-title-area div {
+    padding: 0.5rem 1rem;
+    margin-left: 1rem;
+    margin-right: 1rem;
+    color: white;
+  }
+  div.middle-title-area div.button {
+    background: #0000;
+    border: 1px solid #fff9;
+    border-radius: 5px;
+    cursor: pointer;
+    margin-bottom: 0.5rem;
+    float: left;
+  }
+  div.form-label {
+    width: 30%;
+  }
+  .allblack * {
+    color: black !important;
+  }
+`}`;
+  return (
+    <Layout title="Accordions">
+      <CustomCSS />
+      <Row>
+        <Col className="centerAll" breakPoint={{ xs: 12, md: 12 }}>
+          <img src="/images/wewinlogo_black.png" className="contentHeaderImage" />
+          <div className="header-white">
+            <div className="blue-title">Select</div>
+          </div>
+          <div className="content-area">
+            <table style={{ width: '100%', border: '1px solid white', borderCollapse: 'collapse' }}>
+              <tbody>
+                <tr>
+                  <td>
+                    <div className="grayRow">
+                      <Col breakPoint={{ sm: 12, md: 4 }}>
+                        <div className="form-item" style={{ background: '#0000' }}>
+                          <div className="form-label"> Select SH:</div>
+                          <div className="form-value">
+                            {' '}
+                            <input type="text" />
+                          </div>
+                        </div>
+                      </Col>
+                    </div>
+                    <div className="grayRow">
+                      <Col breakPoint={{ sm: 12, md: 4 }}>
+                        <div className="form-item" style={{ background: '#0000' }}>
+                          <div className="form-label"> Select SSMA:</div>
+                          <div className="form-value">
+                            {' '}
+                            <select>
+                              <option>-- Select --</option>
+                            </select>
+                          </div>
+                        </div>
+                      </Col>
+                    </div>
+                    <div className="grayRow">
+                      <Col breakPoint={{ sm: 12, md: 4 }}>
+                        <div className="form-item" style={{ background: '#0000' }}>
+                          <div className="form-label"> Select SMA:</div>
+                          <div className="form-value">
+                            {' '}
+                            <select>
+                              <option>-- Select --</option>
+                            </select>
+                          </div>
+                        </div>
+                      </Col>
+                    </div>
+                    <div className="grayRow">
+                      <Col breakPoint={{ sm: 12, md: 4 }}>
+                        <div className="form-item" style={{ background: '#0000' }}>
+                          <div className="form-label"> Select MA:</div>
+                          <div className="form-value">
+                            {' '}
+                            <select>
+                              <option>-- Select --</option>
+                            </select>
+                          </div>
+                        </div>
+                      </Col>
+                    </div>
+                    <div className="grayRow">
+                      <Col breakPoint={{ sm: 12, md: 4 }}>
+                        <div className="form-item" style={{ background: '#0000' }}>
+                          <div className="form-label"> Select Agent:</div>
+                          <div className="form-value">
+                            {' '}
+                            <select>
+                              <option>-- Select --</option>
+                            </select>
+                          </div>
+                        </div>
+                      </Col>
+                    </div>
+                    <div className="grayRow">
+                      <Col breakPoint={{ sm: 12, md: 12 }}>
+                        <div className="form-item" style={{ background: '#0000' }}>
+                          <div className="form-label" style={{ width: 100 }}>
+                            {' '}
+                            Select SSMA:
+                          </div>
+                          <div className="form-value allblack">
+                            <div style={{ display: 'flex' }}>
+                              <Radio
+                                name="radio"
+                                onChange={() => {}}
+                                options={[
+                                  {
+                                    value: 'PT 1',
+                                    label: 'PT 1',
+                                    checked: true,
+                                  },
+                                  {
+                                    value: 'PT 2',
+                                    label: 'PT 2',
+                                  },
+                                  {
+                                    value: 'AH Live',
+                                    label: 'AH Live',
+                                  },
+                                  {
+                                    value: 'Outright',
+                                    label: 'Outright',
+                                  },
+                                ]}
+                              />
+                              <Checkbox checked={false} onChange={() => {}}>
+                                Apply changes to all PT groups
+                              </Checkbox>
+                            </div>
+                          </div>
+                        </div>
+                      </Col>
+                    </div>
+                    <div>
+                      <table style={{ width: '100%', marginTop: '3rem' }}>
+                        <tbody>
+                          <tr>
+                            <td style={{ width: '2rem' }}>
+                              <img src="/images/sales/Subtract.png"></img>
+                            </td>
+                            <td style={{ textAlign: 'left' }}>Remark</td>
+                          </tr>
+                          <tr>
+                            <td style={{ height: '1rem' }} />
+                          </tr>
+                          <tr>
+                            <td style={{ textAlign: 'left' }} colSpan={2}>
+                              Please verify the results if you choose to update all bet types or sport types
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="header-white" style={{ marginTop: '1rem' }}>
+            <div className="blue-title">Effective PT</div>
+          </div>
+          <div className="content-area">
+            <table id="contentTableObj">
+              <tbody id="contentTable">
+                <tr>
+                  <td rowSpan={2} style={{ width: '15%' }}>
+                    Username
+                  </td>
+                  <td
+                    rowSpan={2}
+                    style={{ width: '35%', borderLeft: '1px solid black', borderRight: '1px solid black' }}
+                  >
+                    Teams
+                  </td>
+                  <td
+                    colSpan={2}
+                    style={{ width: '20%', borderBottom: '1px solid black', borderRight: '1px solid black' }}
+                  >
+                    First Goal
+                  </td>
+                  <td colSpan={2} style={{ width: '20%', borderBottom: '1px solid black' }}>
+                    First Half
+                  </td>
+                  <td rowSpan={2} style={{ width: '20%', borderLeft: '1px solid black' }}>
+                    No Goal
+                  </td>
+                </tr>
+                <tr>
+                  <td>Home</td>
+                  <td style={{ borderLeft: '1px solid black', borderRight: '1px solid black' }}>Away</td>
+                  <td>Home</td>
+                  <td style={{ borderLeft: '1px solid black' }}>Away</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+          <div className="header-white" style={{ marginTop: '1rem' }}>
+            <div className="blue-title">Effect PT Settings</div>
+          </div>
+          <div className="content-area">
+            <table style={{ width: '100%', border: '1px solid white', borderCollapse: 'collapse' }}>
+              <thead style={{ background: 'url(/images/totalbet/member_total/header.png)', backgroundSize: 'cover' }}>
+                <tr>
+                  <td style={{ color: 'white', height: '44px', width: '35%' }}>Type</td>
+                  <td style={{ color: 'white', width: '33%' }}>Min Value</td>
+                  <td style={{ color: 'white', width: '32%' }}>Max Value</td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td colSpan={12}>
+                    <Col breakPoint={{ sm: 12, md: 4 }}>
+                      <div className="grayRow">
+                        <div className="form-item">
+                          <div className="form-label">Min PT:</div>
+                          <div className="form-value">
+                            <select>
+                              <option></option>
+                            </select>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="grayRow">
+                        <div className="form-item">
+                          <div className="form-label">Forced PT:</div>
+                          <div className="form-value">
+                            <select>
+                              <option></option>
+                            </select>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="grayRow">
+                        <div className="form-item">
+                          <div className="form-label">Is Take All: </div>
+                          <div className="form-value">
+                            <select>
+                              <option></option>
+                            </select>
+                          </div>
+                        </div>
+                      </div>
+                    </Col>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <Button
+            style={{
+              border: '0px',
+              background: 'linear-gradient(89.33deg, #0075FF 0.58%, #00D1FF 104.03%)',
+              color: 'white',
+              width: '150px',
+              marginTop: '1rem',
+              backgroundSize: 'cover',
+              borderRadius: 10,
+            }}
+          >
+            Update
+          </Button>
+        </Col>
+      </Row>
+    </Layout>
+  );
+};
+
+export default AgentReport;
---
+++ src/pages/member/preset_pt1.tsx
@@ -0,0 +1,338 @@
+import Row from '@paljs/ui/Row';
+import Col from '@paljs/ui/Col';
+import React from 'react';
+import Layout from 'Layouts';
+import { createGlobalStyle, css } from 'styled-components';
+import { breakpointDown } from '@paljs/ui/breakpoints';
+import Select from '@paljs/ui/Select';
+// import { Button } from '@paljs/ui/Button';
+import { Radio } from '@paljs/ui/Radio';
+import { Checkbox } from '@paljs/ui/Checkbox';
+
+const SimpleHTOE = () => {
+  const CustomCSS = createGlobalStyle`
+${() => css`
+  .auth-layout .main-content {
+    background-color: #e7e8ee;
+    padding-top: 1rem;
+  }
+  .contentHeaderImage {
+    margin-bottom: 0rem;
+  }
+
+  div.header-white {
+    height: 50px;
+    background: url(/images/white_table_head.png);
+    background-size: cover;
+    display: flex;
+  }
+  div.blue-title {
+    margin: auto;
+    width: 334px;
+    height: 33px;
+    margin-top: 0px;
+    background: url(/images/black_title_bkg.png);
+    background-size: 334px 33px;
+    line-height: 33px;
+    font-weight: bold;
+  }
+
+  div.header-blue {
+    height: 100px;
+    background: url(/images/totalbet/member_total/header.png);
+    background-size: cover;
+    display: flex;
+  }
+  div.white-title {
+    position: absolute;
+    margin: auto;
+    margin-left: 3rem;
+    margin-right: 0rem;
+    padding-left: 3rem;
+    padding-right: 3rem;
+    height: 33px;
+    margin-top: 0px;
+    background: white;
+    background-size: 334px 33px;
+    color: black;
+    line-height: 33px;
+    font-weight: bold;
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
+  }
+  div.cash-option,
+  div.credit-option {
+    color: gray;
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+    border: 2px solid gray;
+    border-top: 0px solid gray;
+    background: #0000;
+  }
+  div.selected {
+    background: url(/images/totalbet/member_total/credit.png);
+    background-size: 100% 100%;
+    color: white;
+    border: 0px solid gray;
+  }
+  #contentTableObj {
+    border: 0px solid black;
+    border-collapse: collapse;
+  }
+  #contentTable tr {
+    height: 35px;
+    line-height: 35px;
+    border-radius: 5px;
+    border: 0px solid white;
+    position: relative;
+  }
+  #contentTable tr {
+    background-color: #d7f5eb;
+    width: 50%;
+    color: #051139;
+  }
+  #contentTableObj,
+  #Main-contentTableObj {
+    width: 100%;
+  }
+  #Main-contentTableObj {
+    height: calc(100vh - 300px);
+    background: white;
+  }
+  #Main-contentTableObj * {
+    color: black;
+  }
+  ${breakpointDown('sm')`
+    div.white-title {
+      margin-left: 0.5rem;
+      margin-right: 0.5rem;
+      padding-left: 0.5rem;
+      padding-right: 0.5rem;
+      float: left;
+    }
+    div.header-blue {
+      border-radius: 7px;
+      height: 130px;
+    }
+    div.header-blue {
+      display: block;
+    }
+  `}
+
+  div.ticket {
+    width: 200px;
+    height: 70px;
+    margin: auto;
+    position: relative;
+    background: linear-gradient(126.97deg, rgba(6, 11, 38, 0.74) 28.26%, rgba(26, 31, 55, 0.5) 91.2%);
+    border-radius: 10px;
+    grid-template-rows: 1fr 1fr;
+    grid-template-columns: 1fr 70px;
+    grid-template-areas: 'small-title right-icon' 'big-title right-icon';
+    display: grid;
+  }
+  div.ticket div.small-title {
+    justify-content: bottom;
+    display: flex;
+    grid-area: small-title;
+    margin-left: 1rem;
+    margin-bottom: 0rem;
+    text-align: left;
+    color: #aaa;
+    justify-content: end;
+    flex-direction: column;
+  }
+  div.ticket div.right-icon {
+    display: flex;
+    background: url(/images/totalbet/member_total/Icon.png);
+    width: 56px;
+    height: 56px;
+    grid-area: right-icon;
+    margin: auto;
+  }
+  div.ticket div.right-icon > img {
+    margin: auto;
+  }
+  div.ticket div.big-title {
+    margin-left: 1rem;
+    margin-top: 0rem;
+    grid-area: big-title;
+    text-align: left;
+    font-weight: bold;
+  }
+  div.select {
+    height: 36px;
+  }
+  div.ticket.select {
+    display: block;
+    margin-bottom: 1rem;
+  }
+  div.content-area {
+    width: 100%;
+    background: white;
+    padding: 0rem 0rem 4rem 0rem;
+  }
+  div.content-area * {
+    color: #051139;
+  }
+  div.grayRow {
+    background: #dceffd;
+    box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.07);
+    border-radius: 10px;
+    width: 100%;
+  }
+  div.grayRow:nth-child(2n + 1) {
+    background: #d7f5eb;
+  }
+  div.grayRow table {
+    width: 100%;
+  }
+  div.mobile-responsive {
+    display: flex;
+  }
+  div.select-mobile {
+    margin-left: 1rem;
+    width: 150px;
+  }
+  ${breakpointDown('sm')`
+    div.mobile-responsive {
+      display: block;
+    }
+    div.select-mobile {
+      width: 100%;
+    }
+  `}
+`}`;
+  return (
+    <Layout title="Accordions">
+      <CustomCSS />
+      <Row>
+        <Col className="centerAll" breakPoint={{ xs: 12, md: 12 }}>
+          <img src="/images/wewinlogo_black.png" className="contentHeaderImage" />
+          <div
+            className="header-blue"
+            style={{ position: 'relative', height: 'auto', borderRadius: 10, paddingBottom: '1.2rem' }}
+          >
+            <div className="white-title" style={{ position: 'relative' }}>
+              Select
+            </div>
+            <div style={{ lineHeight: '40px', marginLeft: '2rem', marginRight: '2rem' }}>PT Settings:</div>
+            <div className="mobile-responsive">
+              <Radio
+                name="radio"
+                onChange={() => {}}
+                options={[
+                  {
+                    value: 'PT 1',
+                    label: 'PT 1',
+                    checked: true,
+                  },
+                  {
+                    value: 'PT 2',
+                    label: 'PT 2',
+                  },
+                  {
+                    value: 'AH Live',
+                    label: 'AH Live',
+                  },
+                  {
+                    value: 'Outright',
+                    label: 'Outright',
+                  },
+                ]}
+              />
+            </div>
+            <Checkbox checked={false} onChange={() => {}}>
+              Apply changes to all PT groups
+            </Checkbox>
+            <div className="select-mobile">
+              <Select placeholder="Sports" options={[]} />
+            </div>
+          </div>
+          <div style={{ height: '1rem' }} />
+          <div className="header-blue" style={{ position: 'relative' }}>
+            <div className="white-title">Preset PT \ Wewin247pro</div>
+            <table style={{ width: '100% ', position: 'absolute', bottom: '1rem' }}>
+              <tbody>
+                <tr>
+                  <td style={{ width: '5%' }}>#</td>
+                  <td style={{ width: '15%' }}>Username</td>
+                  <td style={{ width: '15%' }}>First Name</td>
+                  <td style={{ width: '15%' }}>Last Name</td>
+                  <td style={{ width: '10%' }}>Edit</td>
+                  <td style={{ width: '10%' }}>Min PT</td>
+                  <td style={{ width: '10%' }}>Force PT</td>
+                  <td style={{ width: '10%' }}>Take Remaining</td>
+                  <td style={{ width: '10%' }}>Status</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="content-area">
+            <table style={{ width: '100%', border: '1px solid white', borderCollapse: 'collapse' }}>
+              <tbody>
+                <tr>
+                  <td colSpan={12}>
+                    <div className="grayRow">
+                      <table style={{ borderCollapse: 'collapse' }}>
+                        <tbody>
+                          <tr>
+                            <td style={{ width: '5%', height: '44px' }}>#</td>
+                            <td style={{ width: '15%' }}>Username</td>
+                            <td style={{ width: '15%' }}>First Name</td>
+                            <td style={{ width: '15%' }}>Last Name</td>
+                            <td style={{ width: '10%' }}>Edit</td>
+                            <td style={{ width: '10%' }}>Min PT</td>
+                            <td style={{ width: '10%' }}>Force PT</td>
+                            <td style={{ width: '10%' }}>Take Remaining</td>
+                            <td style={{ width: '10%' }}>Status</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                    <div className="grayRow">
+                      <table style={{ borderCollapse: 'collapse' }}>
+                        <tbody>
+                          <tr>
+                            <td style={{ width: '5%', height: '44px' }}>#</td>
+                            <td style={{ width: '15%' }}>Username</td>
+                            <td style={{ width: '15%' }}>First Name</td>
+                            <td style={{ width: '15%' }}>Last Name</td>
+                            <td style={{ width: '10%' }}>Edit</td>
+                            <td style={{ width: '10%' }}>Min PT</td>
+                            <td style={{ width: '10%' }}>Force PT</td>
+                            <td style={{ width: '10%' }}>Take Remaining</td>
+                            <td style={{ width: '10%' }}>Status</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                    <div className="grayRow">
+                      <table style={{ borderCollapse: 'collapse' }}>
+                        <tbody>
+                          <tr>
+                            <td style={{ width: '5%', height: '44px' }}>#</td>
+                            <td style={{ width: '15%' }}>Username</td>
+                            <td style={{ width: '15%' }}>First Name</td>
+                            <td style={{ width: '15%' }}>Last Name</td>
+                            <td style={{ width: '10%' }}>Edit</td>
+                            <td style={{ width: '10%' }}>Min PT</td>
+                            <td style={{ width: '10%' }}>Force PT</td>
+                            <td style={{ width: '10%' }}>Take Remaining</td>
+                            <td style={{ width: '10%' }}>Status</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </Col>
+      </Row>
+    </Layout>
+  );
+};
+
+export default SimpleHTOE;
